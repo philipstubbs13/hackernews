a2682e34337b672a3df4e634967a50b03849d297
Phil S <philipstubbs13@gmail.com>
2018-06-09 14:44:19 -0500

    basic search working

--- client/src/App.js
+++ client/src/App.js
@@ -20,15 +20,27 @@ const list = [
   },
 ];
 
+// Define higher-order function outside of App component.
+// Takes the searchTerm and returns another function.
+const isSearched = searchTerm => item => 
+    // some condition wich returns true or false.
+    item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       list,
+      searchTerm: '',
     };
 
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
   onDismiss(id) {
@@ -42,7 +54,13 @@ class App extends Component {
 
     return (
       <div className="App">
-        {this.state.list.map(item => 
+        <form>
+          <input 
+            type="text"
+            onChange={this.onSearchChange} 
+          />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
