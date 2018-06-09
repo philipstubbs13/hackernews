a993481f510286f784532bdde05b51c7ad684fae
Phil S <philipstubbs13@gmail.com>
2018-06-09 07:24:26 -0500

    dismiss button

--- client/src/App.js
+++ client/src/App.js
@@ -21,12 +21,28 @@ const list = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
   render() {
 
     return (
       <div className="App">
-        {list.map(function(item) {
-          return (
+        {this.state.list.map(item => 
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -34,9 +50,16 @@ class App extends Component {
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
             </div>
-          );
-        })}
+        )}
       </div>
     );
   }
